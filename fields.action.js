7b6ef8e22597880dabe0586912a43ac6a67b1fc6
Karim Ratib <infojunkie@48424.no-reply.drupal.org>
2009-03-11 01:10:30 +0000

    #358617 by kratib: Select-all checkbox

--- fields.action.js
+++ fields.action.js
@@ -2,15 +2,27 @@
 (function ($) {
 // START jQuery
 
+Drupal.fieldsAction = Drupal.fieldsAction || {};
+
+Drupal.fieldsAction.updateToggler = function(toggler) {
+  var parent = $(toggler).parents('tr')[0];
+  if ($(toggler).is(':checked')) {
+    $('.fields-action-togglable :input', parent).removeAttr('disabled');
+  }
+  else {
+    $('.fields-action-togglable :input', parent).attr('disabled', true);
+  }
+}
+
 $(document).ready(function() {
   $('.fields-action-toggler').click(function() {
-    var parent = $(this).parents('tr')[0];
-    if ($(this).is(':checked')) {
-      $('.fields-action-togglable :input', parent).removeAttr('disabled');
-    }
-    else {
-      $('.fields-action-togglable :input', parent).attr('disabled', true);
-    }
+    Drupal.fieldsAction.updateToggler(this);
+  });
+
+  $('th.select-all').click(function() {
+    $('.fields-action-toggler').each(function() {
+      Drupal.fieldsAction.updateToggler(this);
+    });
   });
   
   // Disable all by default.
