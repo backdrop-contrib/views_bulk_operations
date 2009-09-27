c6148bd1104ec89c88d6ca8b724236ee8cd8b0f4
Karim Ratib <infojunkie@48424.no-reply.drupal.org>
2009-09-27 04:19:28 +0000

    #461050 by kratib: Programmatically execute VBO and expose action to execute it

--- views_bulk_operations.action.js
+++ views_bulk_operations.action.js
@@ -10,10 +10,22 @@ Drupal.vbo.action.updateOperations = function(vid) {
   if (Drupal.settings.vbo.action.views_operations[vid] == undefined) {
     options += "<option value=\"0\">" + Drupal.t("- No operation found in this view -") + "</option>";
   }
-  else $.each(Drupal.settings.vbo.action.views_operations[vid], function(value, text) {
-    options += "<option value=\"" + value + "\">" + text + "</option>\n";
-  });
+  else {
+    options += "<option value=\"0\">" + Drupal.t("- Choose an operation -") + "</option>";
+    $.each(Drupal.settings.vbo.action.views_operations[vid], function(value, text) {
+      options += "<option value=\"" + value + "\">" + text + "</option>\n";
+    });
+  }
   $("#edit-operation-callback").html(options);
+}
+
+Drupal.behaviors.vbo_action = function(context) {
+  vid = $("#edit-view-vid").val();
+  if (vid !=0) {
+    operation = $("#edit-operation-callback").val();
+    Drupal.vbo.action.updateOperations(vid);
+    $("#edit-operation-callback").val(operation);
+  }
 }
 
 // END jQuery
