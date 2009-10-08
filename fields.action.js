e092c6f430d0cc2b45f78e7b02a21cdbbbdb71d8
Karim Ratib <infojunkie@48424.no-reply.drupal.org>
2009-10-08 21:37:55 +0000

    #561358 by kratib: Fixed handling of required profile fields and refactored profile action

--- fields.action.js
+++ fields.action.js
@@ -2,9 +2,10 @@
 (function ($) {
 // START jQuery
 
-Drupal.fieldsAction = Drupal.fieldsAction || {};
+Drupal.vbo = Drupal.vbo || {};
+Drupal.vbo.fieldsAction = Drupal.vbo.fieldsAction || {};
 
-Drupal.fieldsAction.updateToggler = function(toggler) {
+Drupal.vbo.fieldsAction.updateToggler = function(toggler) {
   var parent = $(toggler).parents('tr')[0];
   if ($(toggler).is(':checked')) {
     $('.fields-action-togglable :input', parent).removeAttr('disabled');
@@ -14,20 +15,20 @@ Drupal.fieldsAction.updateToggler = function(toggler) {
   }
 }
 
-$(document).ready(function() {
+Drupal.behaviors.vbo_fieldsAction = function(context) {
   $('.fields-action-toggler').click(function() {
-    Drupal.fieldsAction.updateToggler(this);
+    Drupal.vbo.fieldsAction.updateToggler(this);
   });
 
   $('th.select-all').click(function() {
     $('.fields-action-toggler').each(function() {
-      Drupal.fieldsAction.updateToggler(this);
+      Drupal.vbo.fieldsAction.updateToggler(this);
     });
   });
   
   // Disable all by default.
   $('.fields-action-togglable :input').attr('disabled', true);
-});
+}
 
 // END jQuery
 })(jQuery);
