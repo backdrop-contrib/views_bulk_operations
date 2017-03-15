57bb8af4542b727c1e6c6854e7176322747ef6fa
claudiucristea <claudiucristea@56348.no-reply.drupal.org>
2017-03-15 10:40:25 -0700

    Issue #2860320 by claudiu.cristea, joelpittet: Allow exceptions when toggling buttons

--- js/views_bulk_operations.js
+++ js/views_bulk_operations.js
@@ -147,7 +147,9 @@
     // If no rows are checked, disable any form submit actions.
     var selectbox = $('select[name="operation"]', form);
     var checkedCheckboxes = $('.vbo-select:checked', form);
-    var buttons = $('[id^="edit-select"] input[type="submit"]', form);
+    // The .vbo-prevent-toggle CSS class is added to buttons to prevent toggling
+    // between disabled and enabled. For example the case of an 'add' button.
+    var buttons = $('[id^="edit-select"] input[type="submit"]:not(.vbo-prevent-toggle)', form);
 
     if (selectbox.length) {
       var has_selection = checkedCheckboxes.length && selectbox.val() !== '0';
